9502932a836f953129ffc7988e21b8e773093538
Sengphachanh.dev <sengphachanh.dev@gmail.com>
2024-01-21 20:15:42 +0700

    chore: update metadata in layout.tsx and config.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,36 @@
-'use client'
-
-import { Helmet, HelmetProvider } from 'react-helmet-async'
-
-import { METADATA } from '@/constants/config'
+import { CONFIG } from '@/constants'
 import MatrixBackground from '@/components/matrix-background'
 import { Providers } from '@/app/providers'
 
 import './globals.css'
+
+export const metadata = {
+  title: 'Sengphachanh Chanthavong | Front-end Developer',
+  description:
+    'Explore the personal website of Sengphachanh Chanthavong, a passionate Front-end Developer. Discover my projects, skills, and get in touch today.',
+  keywords: [
+    'Sengphachanh Chanthavong',
+    'Tyecode',
+    'Front-end Developer',
+    'Web Development',
+    'Personal Website',
+  ],
+  author: 'Sengphachanh Chanthavong, tyecode',
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+  alternates: {
+    canonical: CONFIG.url,
+  },
+}
 
 export default function RootLayout({
   children,
@@ -15,27 +39,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <HelmetProvider>
-        <Helmet>
-          <title>{METADATA.title}</title>
-          <meta name="author" content="tyecode <github.com/tyecode" />
-          <meta name="description" content={METADATA.description} />
-          <meta name="keywords" content={METADATA.keywords.join(',')} />
-          <meta name="robots" content="index, follow" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <link rel="canonical" href="https://www.tyecode.space/" />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        </Helmet>
-
-        <body suppressHydrationWarning>
-          <MatrixBackground />
-          <div className="fixed top-0 -z-40 h-screen w-screen bg-background/80"></div>
-          <Providers>{children}</Providers>
-        </body>
-      </HelmetProvider>
+      <body suppressHydrationWarning>
+        <MatrixBackground />
+        <div className="fixed top-0 -z-40 h-screen w-screen bg-background/80"></div>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
--- constants/config.ts
+++ constants/config.ts
@@ -1,22 +1,7 @@
 export const CONFIG = {
+  url: 'https://tyecode.space/',
   name: 'Sengphachanh Chanthavong',
   skill: 'Front-End Developer',
   description:
     'Creating smart user interfaces, developing rich web applications, and delivering seamless web experiences.',
-}
-
-export const METADATA = {
-  title: CONFIG.name + ' - Personal Website',
-  keywords: [
-    'personal',
-    'website',
-    'tyecode',
-    'sengphachanh',
-    'chanthavong',
-    'developer',
-    'frontend',
-    'code',
-  ],
-  description:
-    'Sengphachanh Chanthavong or Tyecode - A Front-end Developer passionate about Web development. Explore my personal website and contact me today.',
 }
