06bd0d13ee1bf4b8744738d01712b9e4af7552d7
Sengphachanh.dev <sengphachanh.dev@gmail.com>
2024-01-21 19:58:04 +0700

    chore: update layout and metadata in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import { CONFIG } from '@/constants'
+'use client'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+
+import { METADATA } from '@/constants/config'
 import MatrixBackground from '@/components/matrix-background'
 import { Providers } from '@/app/providers'
 
 import './globals.css'
-
-export const metadata = {
-  title: CONFIG.name + ' - Personal Website',
-  keywords: ['personal', 'website', 'tyecode', 'sengphachanh', 'chanthavong'],
-  description:
-    'Sengphachanh Chanthavong or Tyecode - A Front-end Developer passionate about Web development. Explore my personal website and contact me today.',
-}
 
 export default function RootLayout({
   children,
@@ -18,11 +15,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body suppressHydrationWarning>
-        <MatrixBackground />
-        <div className="fixed top-0 -z-40 h-screen w-screen bg-background/80"></div>
-        <Providers>{children}</Providers>
-      </body>
+      <HelmetProvider>
+        <Helmet>
+          <title>{METADATA.title}</title>
+          <meta name="author" content="tyecode <github.com/tyecode" />
+          <meta name="description" content={METADATA.description} />
+          <meta name="keywords" content={METADATA.keywords.join(',')} />
+          <meta name="robots" content="index, follow" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="canonical" href="https://www.tyecode.space/" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        </Helmet>
+
+        <body suppressHydrationWarning>
+          <MatrixBackground />
+          <div className="fixed top-0 -z-40 h-screen w-screen bg-background/80"></div>
+          <Providers>{children}</Providers>
+        </body>
+      </HelmetProvider>
     </html>
   )
 }
--- constants/config.ts
+++ constants/config.ts
@@ -4,3 +4,19 @@ export const CONFIG = {
   description:
     'Creating smart user interfaces, developing rich web applications, and delivering seamless web experiences.',
 }
+
+export const METADATA = {
+  title: CONFIG.name + ' - Personal Website',
+  keywords: [
+    'personal',
+    'website',
+    'tyecode',
+    'sengphachanh',
+    'chanthavong',
+    'developer',
+    'frontend',
+    'code',
+  ],
+  description:
+    'Sengphachanh Chanthavong or Tyecode - A Front-end Developer passionate about Web development. Explore my personal website and contact me today.',
+}
